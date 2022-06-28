dec17c33d0c799aea67a747b0084fddfd1bb363d
Pandiyan <pandiec@gmail.com>
2022-06-28 15:13:29 +0530

    Adding ACL=private

--- meta-arguments-for_each/s3bucket.tf
+++ meta-arguments-for_each/s3bucket.tf
@@ -12,15 +12,11 @@ resource "aws_s3_bucket" "mys3bucket" {
   }
 
   bucket = "${each.key}-${each.value}"
+  acl    = "private"
   
   tags = {
     Environment = each.key
     bucketname  = "${each.key}-${each.value}"
     eachvalue   = each.value
   }
-}
-
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
-  bucket = aws_s3_bucket.mys3bucket.${each.key}-${each.value}
-  acl    = "private"
 }
