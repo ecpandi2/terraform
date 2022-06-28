5198d075ecd95fc43c6fd38b5800165d8684d2ca
Pandiyan <pandiec@gmail.com>
2022-06-28 16:52:31 +0530

    Changed the availability zone

--- meta-lifecycle_create_destroy/ec2instance.tf
+++ meta-lifecycle_create_destroy/ec2instance.tf
@@ -2,8 +2,8 @@
 resource "aws_instance" "web" {
   ami               = "ami-0915bcb5fa77e4892" # Amazon Linux
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
+  #availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     "Name" = "web-1"
   }
