14eb0626a4ce274b48cd3b1d1bb667c990421a64
Pandiyan <pandiec@gmail.com>
2022-06-29 06:27:56 +0530

    Input-Variables-Maps

--- Input-Variables-Maps/ec2-instance.tf
+++ Input-Variables-Maps/ec2-instance.tf
@@ -1,7 +1,7 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
   ami                    = var.ec2_ami_id
-  instance_type          = var.ec2_instance_type[1]
+  instance_type          = instance_type = var.ec2_instance_type_map["big-apps"]
   key_name               = "terraform-key"
   count                  = var.ec2_instance_count
   user_data              = <<-EOF
@@ -13,7 +13,5 @@ resource "aws_instance" "my-ec2-vm" {
     echo "<h1>Welcome to LynxKloud ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
-  tags = {
-    "Name" = "myec2vm"
-  }
+  tags = var.ec2_instance_tags
 }
