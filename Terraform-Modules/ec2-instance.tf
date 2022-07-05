894d75aa3839476cc54a98e8c9b814427d87ed93
Pandiyan <pandiec@gmail.com>
2022-07-05 08:11:41 +0530

    Terraform Modules

---
+++ Terraform-Modules/ec2-instance.tf
@@ -0,0 +1,21 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 4.0"
+
+  name = "Modules-Demo-Server"
+  instance_count         = 2
+
+  ami                    = data.aws_ami.amzlinux.id
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-07eecdd15a2c89037"]
+  subnet_id              = "subnet-0b858b1296da80504"
+  user_data              = file("apache-install.sh") 
+
+  tags = {
+    Name        = "Modules-Demo-Server"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
